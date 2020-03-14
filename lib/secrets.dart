26168d4696d3e28ceb4db1ce17f40e223c138283
ulrich mabou <13383417+ulrichmabou@users.noreply.github.com>
2020-03-14 20:53:37 +0300

    remove secrets

--- lib/secrets.dart
+++
@@ -1,4 +0,0 @@
-const consumerApiKey = "zLz8w60hWLidw3zEtFwENHeIO";
-const consumerApiSecret = "DKauqpBRzU1751TPExIdku96pKWq7AbbtpVxEBOq73gC0LdaLe";
-const accessToken = "1603070526-FfSvB34q9XcqTCV6TCqF7iGQpi2eNmGCEbTnmXk";
-const accessTokenSecret = "ipDxBnnwQ2uetl26IGYz3OqsB2a2WYMF1lPUCd0lRSVTa";
